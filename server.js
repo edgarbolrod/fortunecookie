cddac3e3e4a929b89f68f06bcde75071a5876641
edgarbolrod <isibolanos.19@gmail.com>
2016-11-01 16:06:34 -0600

    server html basico funcionando

--- server.js
+++ server.js
@@ -1,14 +1,25 @@
 var http = require ('http');
 
+var colors = require ('colors');
+//estableciendo tema de colores
+
+colors.setTheme({
+    'data':'cyan',
+    'info': 'rainbow',
+    'error':'red'
+});
+
 var server = http.createServer(function(req, res){
     res.writeHead(200, {
-        'Content-Type':'text/plain'
+        'Content-Type':'text/html'
     });
 
-    res.end('Server Funcionando...');
+    res.write('<h1>Server Funcionando</h1>');
+    res.write('<p>mi sitio asombroso</p>')
+    res.end();
 
 });
 
 server.listen(3000, '127.0.0.1', function(){
-    console.log('>server corriendo en http://127.0.0.1:3000...');
+    console.log('>server corriendo en http://127.0.0.1:3000...'.info);
 });
