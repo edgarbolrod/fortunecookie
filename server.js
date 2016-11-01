cc96a913268c2c4da7da1ab5ba060ea01abe58b0
edgarbolrod <isibolanos.19@gmail.com>
2016-11-01 16:38:06 -0600

    Server Html lectura de archivo

--- server.js
+++ server.js
@@ -1,25 +1,39 @@
-var http = require ('http');
+var http = require('http');
+var fs = require('fs');
 
-var colors = require ('colors');
+var colors = require('colors');
 //estableciendo tema de colores
 
 colors.setTheme({
-    'data':'cyan',
+    'data': 'cyan',
     'info': 'rainbow',
-    'error':'red'
+    'error': 'red'
 });
 
-var server = http.createServer(function(req, res){
-    res.writeHead(200, {
-        'Content-Type':'text/html'
-    });
+var server = http.createServer(function (req, res) {
+    fs.readFile('./static/index.html', 'utf8',
+        function (err, content) {
 
-    res.write('<h1>Server Funcionando</h1>');
-    res.write('<p>mi sitio asombroso</p>')
-    res.end();
+            if (err) {//hubo error
+                res.writeHead(500,{
+                    'Content-Type':"text/html"
 
+                });
+                console.log('>error en la lectura de'.error +
+                'un archivo: ln20 server.js'.error);
+
+                res.end("<h1>Error Interno</h1>")
+
+            } else {//no hubo error
+                res.writeHead(200, {
+                    'Content-Type':'text/html'
+                });
+                console.log('>sirviendo index.html');
+                res.end(content);
+            }
+        });
 });
 
-server.listen(3000, '127.0.0.1', function(){
+server.listen(3000, '127.0.0.1', function () {
     console.log('>server corriendo en http://127.0.0.1:3000...'.info);
 });
