dcbf0c327527b129cb4a9583f467b4aec49e174e
mariana zayas jimenez <marianazj.301294@gmail.com>
2016-11-18 04:33:11 +0000

    server modular

---
+++ internals/static-server.js
@@ -0,0 +1,35 @@
+
+exports.serve=function(res,url){
+var fs = require('fs');
+var mime = require('mime');
+
+
+
+
+    var filePath='./static'+ url;
+    console.log(`>Se servira Archivo: ${filePath}`.data);
+    //seleccionar el tipo mime
+    var mimeType = mime.lookup(filePath);
+    fs.readFile(filePath,
+        function (err, content) {
+
+            if (err) {//hubo error
+                res.writeHead(500, {
+                    'Content-Type': "text/html"
+
+                });
+                console.log('>error en la lectura de'.error +
+                    'un archivo: ln20 server.js'.error);
+
+                res.end("<h1>Error Interno</h1>")
+
+            } else {//no hubo error
+                res.writeHead(200, {
+                    'Content-Type': mimeType
+                });
+                console.log(`>sirviendo: ${filePath}`.data );
+                res.end(content);
+            }
+        });
+    
+};
--- server.js
+++ server.js
@@ -2,6 +2,7 @@ var http = require('http');
 var fs = require('fs');
 var mime = require('mime');
 var colors = require('colors');
+var staticServer = require ("./internals/static-server.js");
 
 //iportando el puerto configurador
 var config = require('./config/config');
@@ -23,31 +24,7 @@ var server = http.createServer(function (req, res) {
 
 
     console.log(`>Recurso solicitado >${url}`.data);
-    var filePath='./static'+ url;
-    console.log(`>Se servira Archivo: ${filePath}`.data);
-    //seleccionar el tipo mime
-    var mimeType = mime.lookup(filePath);
-    fs.readFile(filePath,
-        function (err, content) {
 
-            if (err) {//hubo error
-                res.writeHead(500, {
-                    'Content-Type': "text/html"
-
-                });
-                console.log('>error en la lectura de'.error +
-                    'un archivo: ln20 server.js'.error);
-
-                res.end("<h1>Error Interno</h1>")
-
-            } else {//no hubo error
-                res.writeHead(200, {
-                    'Content-Type': mimeType
-                });
-                console.log(`>sirviendo: ${filePath}`.data );
-                res.end(content);
-            }
-        });
 });
 
 server.listen(PORT,IP, function () {
